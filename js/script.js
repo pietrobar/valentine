63eb2d80b3a4e450e5476f6e0f3d5cf7159e7502
pGYM <pietro98x@hotmail.it>
2026-02-02 15:49:48 +0100

    inizializzazione pulsanti

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Pagina caricata con successo!');
+
+    const noButton = document.getElementById('no-button');
+
+    noButton.addEventListener('mouseover', () => {
+        const buttonRect = noButton.getBoundingClientRect();
+
+        const maxX = window.innerWidth - buttonRect.width;
+        const maxY = window.innerHeight - buttonRect.height;
+
+        const randomX = Math.random() * maxX;
+        const randomY = Math.random() * maxY;
+
+        noButton.style.left = `${randomX}px`;
+        noButton.style.top = `${randomY}px`;
+    });
 });
